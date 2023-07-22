161bc559ed5e1bec7740333909dab43ad64dc790
enfyna <legendraslan@gmail.com>
2023-07-22 12:03:36 +0300

    Minify Bootstrap CSS with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require('gulp');
 const { series } = require('gulp');
 const htmlmin = require('gulp-htmlmin');
 const terser = require('gulp-terser');
+const purify = require('gulp-purifycss');
+const cleanCSS = require('gulp-clean-css');
 
 function minifyHTML() {
     return gulp.src('src/index.html')
@@ -15,4 +17,14 @@ function minifyJS() {
         .pipe(gulp.dest('tsc'));
 }
 
+function minifyBootstrapCSS() {
+    return gulp.src('bootstrap/bootstrap.css')
+        .pipe(purify(['src/index.html', 'src/script.ts'], {
+            whitelist : ['collapsing','show']
+        }))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('tsc'));
+}
+
 exports.minify = series(minifyHTML, minifyJS);
+exports.minifyCSS = minifyBootstrapCSS;
