c35f7110b41cf134e8c21b7aa5d20e0016c1c2b4
enfyna <legendraslan@gmail.com>
2023-10-12 20:58:27 +0300

    Add translations to toolbar icon title.

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,24 +1,50 @@
 let is_tab_saved : boolean = false;
+
+async function translate(){
+	const translations = {
+		"tr": "Bu sayfayı kısayol olarak kaydet",
+		"de": "Diese Seite als Verknüpfung speichern",
+		"en": "Save this page as a shortcut",
+		"es": "Guardar esta página como acceso directo"
+	};	  
+	let lang : string;
+	switch (navigator.language.toLowerCase().split("-")[0]){
+		case "tr":
+			lang = "tr";
+			break;
+		case "de":
+			lang = "de";
+			break;
+		case "es":
+			lang = "es";
+			break;
+		default:
+			lang = "en";
+			break;
+	};
+	await browser.browserAction.setTitle({
+		title:translations[lang]
+	});
+}
+
+translate();
+
 async function setActiveTabAsShortcut() {
-	const currentTab : browser.tabs.Tab = (await browser.tabs.query(
-		{
-			active: true,
-			currentWindow: true
-		}
-	)).reverse().pop();
+	const currentTab : browser.tabs.Tab = (await browser.tabs.query({
+		active: true,
+		currentWindow: true
+	})).reverse().pop();
 
 	if(!currentTab){
 		return;
 	}
 
 	const save = await browser.storage.local.get();
-	save['shortcuts'].push(
-		{
-			name:currentTab.title,
-			link:currentTab.url,
-			img:currentTab.favIconUrl ?? ''
-		}
-	);
+	save['shortcuts'].push({
+		name:currentTab.title,
+		link:currentTab.url,
+		img:currentTab.favIconUrl ?? ''
+	});
 	localStorage.clear();
 	await browser.storage.local.set(save);
 	await browser.browserAction.setIcon({
