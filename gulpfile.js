2948f4b98aa44b6846395032ad3a88dc0e9717a3
enfyna <legendraslan@gmail.com>
2024-06-27 12:32:06 +0300

    Minify Json and svg files.

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,12 @@ const terser = require('gulp-terser');
 const purify = require('gulp-purifycss');
 const rename = require('gulp-rename');
 const cleanCSS = require('gulp-clean-css');
+const replace = require('gulp-replace');
+const jsonMin = require('gulp-json-minify');
+const svgMin = require('gulp-svgmin')
 
 function minifyIndex() {
-    return gulp.src('src/index/index.html')
+    return gulp.src('src/html/index.html')
         .pipe(htmlmin({
             collapseWhitespace: true,
             removeComments: true,
@@ -16,11 +19,11 @@ function minifyIndex() {
             collapseBooleanAttributes: true,
             minifyCSS: true,
         }))
-        .pipe(gulp.dest('.'));
+        .pipe(gulp.dest('./build'));
 }
 
 function minifySettings() {
-    return gulp.src('src/settings/settings.html')
+    return gulp.src('src/html/settings.html')
         .pipe(htmlmin({
             collapseWhitespace: true,
             removeComments: true,
@@ -29,11 +32,13 @@ function minifySettings() {
             collapseBooleanAttributes: true,
             minifyCSS: true,
         }))
-        .pipe(gulp.dest('.'));
+        .pipe(gulp.dest('./build'));
 }
 
-function minifyIndexJS() {
-    return gulp.src('tsc/script.js')
+async function minifyIndexJS() {
+    const del = await import('del')
+    return gulp.src('build/js/script.js')
+        .pipe(replace('module.exports = {};', ''))
         .pipe(terser({
             keep_fnames: false,
             mangle: {
@@ -41,11 +46,16 @@ function minifyIndexJS() {
                 keep_fnames: false,
             },
         }))
-        .pipe(gulp.dest('build'));
+        .pipe(gulp.dest('build/js'))
+        .on('end', () =>
+            del.deleteSync('build/script.js')
+        );
 }
 
-function minifySettingsJS() {
-    return gulp.src('tsc/settings.js')
+async function minifySettingsJS() {
+    const del = await import('del')
+    return gulp.src('build/js/settings.js')
+        .pipe(replace('module.exports = {};', ''))
         .pipe(terser({
             keep_fnames: false,
             mangle: {
@@ -53,11 +63,16 @@ function minifySettingsJS() {
                 keep_fnames: false,
             },
         }))
-        .pipe(gulp.dest('build'));
+        .pipe(gulp.dest('build/js'))
+        .on('end', () =>
+            del.deleteSync('build/settings.js')
+        );
 }
 
-function minifyBackgroundJS() {
-    return gulp.src('tsc/background.js')
+async function minifyBackgroundJS() {
+    const del = await import('del')
+    return gulp.src('build/js/background.js')
+        .pipe(replace('module.exports = {};', ''))
         .pipe(terser({
             keep_fnames: false,
             mangle: {
@@ -65,12 +80,15 @@ function minifyBackgroundJS() {
                 keep_fnames: false,
             },
         }))
-        .pipe(gulp.dest('build'));
+        .pipe(gulp.dest('build/js'))
+        .on('end', () =>
+            del.deleteSync('build/background.js')
+        );
 }
 
 function minifyIndexBootstrapCSS() {
     return gulp.src('bootstrap/bootstrap.css')
-        .pipe(purify(['src/index/index.html'], {
+        .pipe(purify(['src/html/index.html'], {
             whitelist: [
                 'rounded-circle',
                 'bg-black',
@@ -78,26 +96,26 @@ function minifyIndexBootstrapCSS() {
         }))
         .pipe(cleanCSS({ level: { 1: { all: true }, 2: { all: true } } }))
         .pipe(rename('bootstrap.css'))
-        .pipe(gulp.dest('build'));
+        .pipe(gulp.dest('build/css'));
 }
 
 function minifyIndexCSS() {
-    return gulp.src('src/index/index.css')
+    return gulp.src('src/css/index.css')
         .pipe(cleanCSS({ level: { 1: { all: true }, 2: { all: true } } }))
         .pipe(rename('index.css'))
-        .pipe(gulp.dest('build'));
+        .pipe(gulp.dest('build/css'));
 }
 
 function minifyColorsCSS() {
-    return gulp.src('src/colors.css')
+    return gulp.src('src/css/colors.css')
         .pipe(cleanCSS({ level: { 1: { all: true }, 2: { all: true } } }))
         .pipe(rename('colors.css'))
-        .pipe(gulp.dest('build'));
+        .pipe(gulp.dest('build/css'));
 }
 
 function minifySettingsCSS() {
     return gulp.src('bootstrap/bootstrap.css')
-        .pipe(purify(['src/settings/settings.html'], {
+        .pipe(purify(['src/html/settings.html'], {
             whitelist: [
                 // input groups
                 'dropdown-toggle',
@@ -114,19 +132,46 @@ function minifySettingsCSS() {
         }))
         .pipe(cleanCSS({ level: { 1: { all: true }, 2: { all: true } } }))
         .pipe(rename('settings.css'))
-        .pipe(gulp.dest('build'));
+        .pipe(gulp.dest('build/css'));
 }
 
-exports.minify = series(
+function Assets() {
+    return gulp.src('./src/assets/icons/*')
+        .pipe(svgMin())
+        .pipe(gulp.dest('./build/assets/icons'));
+}
+
+function minifyManifest() {
+    return gulp.src('./manifest.json')
+        .pipe(jsonMin())
+        .pipe(gulp.dest('./build'));
+}
+
+exports.minifyHTML = series(
     minifyIndex,
-    minifyIndexJS,
-    minifyIndexCSS,
     minifySettings,
-    minifySettingsJS,
-    minifyBackgroundJS,
 );
 exports.minifyCSS = series(
     minifyIndexBootstrapCSS,
+    minifyIndexCSS,
     minifyColorsCSS,
     minifySettingsCSS,
+);
+exports.minifyJS = series(
+    minifyIndexJS,
+    minifySettingsJS,
+    minifyBackgroundJS,
+);
+exports.minifyFull = series(
+    minifyManifest,
+    Assets,
+    minifyIndex,
+    minifySettings,
+    minifyIndexBootstrapCSS,
+    minifyIndexCSS,
+    minifyColorsCSS,
+    minifySettingsCSS,
+    minifyIndexJS,
+    minifySettingsJS,
+    minifyBackgroundJS,
 );
